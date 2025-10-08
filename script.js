999470ce9aba0115eeebe159a2cd9f8c1058fd50
Pedro Antonny <123685133+PedroAntonny@users.noreply.github.com>
2025-10-08 10:31:31 -0300

    feat: add script and content layout in sidebar

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const elements = {
   promptContent: document.getElementById('prompt-content'),
   titleWrapper: document.getElementById('title-wrapper'),
   contentWrapper: document.getElementById('content-wrapper'),
+  btnOpen: document.getElementById('btn-open'),
+  btnCollapse: document.getElementById('btn-collapse'),
+  sidebar: document.querySelector('.sidebar'),
+  app: document.querySelector('.app'),
 }
 
 function updateEditableWrapperState(element, wrapper) {
@@ -26,8 +30,22 @@ function attachAllEditableHandlers() {
   })
 }
 
+function openSidebar() {
+  elements.app.classList.remove('sidebar-closed')
+}
+
+function closeSidebar() {
+  elements.app.classList.add('sidebar-closed')
+}
+
+function attachSidebarHandlers() {
+  elements.btnOpen.addEventListener('click', openSidebar)
+  elements.btnCollapse.addEventListener('click', closeSidebar)
+}
+
 function init() {
   attachAllEditableHandlers()
+  attachSidebarHandlers()
   updateAllEditableStates()
 }
 
